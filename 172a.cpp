f536174309d65da105d68a6fd642f095f0f46c0d
Ruslan <37127343+RusS1103@users.noreply.github.com>
2020-03-04 11:18:16 +0300

    Create 172a.cpp

---
+++ 172a.cpp
@@ -0,0 +1,17 @@
+const int ZERO = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0;
+    while (n > 0) {
+        n /= 5;
+        sum += n;
+    }
+    return sum;
+    }
+};
