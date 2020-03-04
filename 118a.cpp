b2042e639fb5791ec3c62468eee83f82941e2cd6
Ruslan <37127343+RusS1103@users.noreply.github.com>
2020-03-04 19:22:00 +0300

    Create 118a.cpp

---
+++ 118a.cpp
@@ -0,0 +1,27 @@
+const int ZERO = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+    
+
+class Solution {
+public:
+    
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> Pascal;
+    if (numRows == 0) {
+        return Pascal;
+    } else {
+        Pascal = {{1}};
+        while (--numRows) {
+            vector<int> total(Pascal.size() + 1, 1);
+            for (int i = 1; i < total.size() - 1; ++i) {
+                total[i] = Pascal.back()[i - 1] + Pascal.back()[i];
+            }
+            Pascal.push_back(total);
+        }
+        return Pascal;
+    }
+}
+};
