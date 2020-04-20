e7a98f6b43333a041d7180b488da9caef5c7cd6c
Ruslan <37127343+RusS1103@users.noreply.github.com>
2020-04-20 23:05:14 +0300

    Create a.cpp

---
+++ 111/a.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+const int ZERO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        if (root->left == NULL) {
+            return minDepth(root->right) + 1;
+        }
+        if (root->right == NULL) {
+            return minDepth(root->left) + 1;
+        }
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
