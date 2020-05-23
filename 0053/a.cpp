3cbc46c1b10e526a25463c43fc63a5f9766f0dbb
Ruslan <37127343+RusS1103@users.noreply.github.com>
2020-05-24 00:34:52 +0300

    Update a.cpp

--- 0053/a.cpp
+++ 0053/a.cpp
@@ -1,9 +1,21 @@
+const int ZERO = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
-        stringstream ss(s);
-        string line;
-        while (ss >> line) {}
-        return line.size();
+    int maxSubArray(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        int sum = nums[0];
+        int best = nums[0];
+        for (size_t i = 1; i < nums.size(); ++i) {
+            sum = max(nums[i], nums[i] + sum);
+            best = max(best, sum);
+        }
+        return best;
     }
 };
