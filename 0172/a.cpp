980ea43addec40197836af9fcab27d56e2661531
Ruslan <37127343+RusS1103@users.noreply.github.com>
2020-06-08 00:52:53 +0300

    Update a.cpp

--- 0172/a.cpp
+++ 0172/a.cpp
@@ -8,10 +8,10 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         int sum = 0;
-    while (n > 0) {
-        n /= 5;
-        sum += n;
-    }
-    return sum;
+        while (n > 0) {
+            n /= 5;
+            sum += n;
+        }
+        return sum;
     }
 };
