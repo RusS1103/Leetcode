29a255d560f012dda819890960bd6c968852bc64
Ruslan <37127343+RusS1103@users.noreply.github.com>
2020-06-20 00:13:21 +0300

    Create a.cpp

---
+++ 0167/a.cpp
@@ -0,0 +1,22 @@
+const int ZERO = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0, high = numbers.size() - 1;
+        while (low < high) {
+            if (numbers[low] + numbers[high] == target) {
+                return {low + 1, high + 1};
+            } else if (numbers[low] + numbers[high] > target) {
+                --high;
+            } else {
+                ++low;
+            }
+        }
+        return {};
+    }
+};
