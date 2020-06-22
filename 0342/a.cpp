4f9aab1d7f2422aa718876e8826d89b7f49a2486
Ruslan <37127343+s1td1kov@users.noreply.github.com>
2020-06-22 22:44:08 +0300

    Create a.cpp

---
+++ 0342/a.cpp
@@ -0,0 +1,18 @@
+const int ZERO = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num % 4 == 0) {
+            num /= 4;
+        }
+        return num == 1;
+    }
+};
