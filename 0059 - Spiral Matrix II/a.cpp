440c49aa9bc026838d4ca462590ed0866a5fc51a
Ruslan <37127343+s1td1kov@users.noreply.github.com>
2020-07-05 21:22:09 +0300

    Create a.cpp

---
+++ 0059 - Spiral Matrix II/a.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n, vector<int>(n));
+        int k = 1;
+        int rowBegin = 0;
+        int rowEnd = matrix.size() - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].size() - 1;
+        while (k <= n * n) {
+            for (int i = colBegin; i <= colEnd; ++i) {
+                matrix[rowBegin][i] = k;
+                ++k;
+            }
+            
+            ++rowBegin;
+            
+            for (int i = rowBegin; i <= rowEnd; ++i) {
+                matrix[i][colEnd] = k;
+                ++k;
+            }
+            
+            --colEnd;
+            
+            if (k <= n * n) {
+                for (int i = colEnd; i >= colBegin; --i) {
+                    matrix[rowEnd][i] = k;
+                    ++k;
+                }
+            
+            --rowEnd;
+            
+            if (k <= n * n) {
+                for (int i = rowEnd; i >= rowBegin; --i) {
+                    matrix[i][colBegin] = k;
+                    ++k;
+                }
+            }
+            
+            ++colBegin;
+        }
+        return matrix;
+    }
+};
