9ab8d8c7e7efb5c313bbe2fb4c354405879439e8
Ruslan <37127343+s1td1kov@users.noreply.github.com>
2020-10-18 20:28:18 +0300

    Update Search in Rotated Sorted Array.cpp

--- 0033 - Search in Rotated Sorted Array/Search in Rotated Sorted Array.cpp
+++ 0033 - Search in Rotated Sorted Array/Search in Rotated Sorted Array.cpp
@@ -1,40 +1,26 @@
-const int ZERO = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
-}();
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         if (nums.size() == 0) {
             return -1;
         }
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        int start = left;
-        left = 0, right = nums.size() - 1;
-        if (nums[start] <= target && nums[right] >= target) {
-            left = start;
-        } else {
-            right = start;
-        }
+        int left = 0, right = nums.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
+            } else if (nums[mid] >= nums[left]) {
+                if (target <= nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else {
-                right = mid - 1;
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
