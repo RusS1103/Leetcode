89780cf9a346087726c745cdd85b9a0240f18d39
Ruslan <37127343+s1td1kov@users.noreply.github.com>
2021-01-22 12:02:08 +0300

    Create Palindrome Number.py

---
+++ 0009 - Palindrome Number/Palindrome Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 : return False
+        s = str(x)
+        rev_s = ""
+        for i in range(len(s) - 1, -1, -1):
+            rev_s = rev_s + s[i]
+        return s == rev_s
