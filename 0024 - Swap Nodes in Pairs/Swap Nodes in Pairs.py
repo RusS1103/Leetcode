10e32da12b8b7fccbd6b4b54d1356d940e504f49
Ruslan <37127343+s1td1kov@users.noreply.github.com>
2021-02-13 21:54:51 +0300

    Create Swap Nodes in Pairs.py

---
+++ 0024 - Swap Nodes in Pairs/Swap Nodes in Pairs.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        prev = dummy = ListNode(0)
+        prev.next = head
+        
+        while prev.next and prev.next.next:
+            a = prev.next
+            b = prev.next.next
+            c = prev.next.next.next
+            prev.next = b
+            prev.next.next = a
+            prev.next.next.next = c
+            prev = prev.next.next
+            
+        return dummy.next
