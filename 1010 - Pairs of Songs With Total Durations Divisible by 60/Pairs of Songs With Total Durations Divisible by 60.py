acefd555ea3137cce1c3dc1e2f7c494adcc35ae7
Ruslan <37127343+s1td1kov@users.noreply.github.com>
2021-04-09 13:40:12 +0300

    Create Pairs of Songs With Total Durations Divisible by 60.py

---
+++ 1010 - Pairs of Songs With Total Durations Divisible by 60/Pairs of Songs With Total Durations Divisible by 60.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        remainders = [0] * 60
+        count = 0
+        for t in time:
+            if t % 60 == 0:
+                count += remainders[0]
+            else:
+                count += remainders[60 - t % 60]
+            remainders[t % 60] += 1
+        return count
+
